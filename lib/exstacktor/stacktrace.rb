ce7e170eb06de655ab53706fd78fddc87e5c8ef9
ronaldsm <ronaldsm@cisco.com>
2019-09-13 16:46:41 -0600

    Add basic include/exclude and tests

--- lib/exstacktor/stacktrace.rb
+++ lib/exstacktor/stacktrace.rb
@@ -1,14 +1,71 @@
 module Exstacktor
   class Stacktrace
     attr_reader :raw_stack
-    attr_writer :max_stack
+    attr_reader :parsed_stack
+    attr_accessor :exclude
+    attr_accessor :include
+    attr_writer :clean_keep
 
-    def initialize(raw_stack)
-      @raw_stack = raw_stack
+    def initialize(exclude: nil, include: nil)
+      @raw_stack = nil
+      @parsed_stack = nil
+      @exclude = exclude
+      @include = include
+      @remove = nil
     end
 
-    def echo(val = 'default string')
-      string
+    def parse(stacktrace)
+      @raw_stack = stacktrace
+      @parsed_stack = []
+      @raw_stack.each do |line|
+        if exclude_line?(line)
+          next unless include_line?(line) # any override to the exclusion?
+        end
+
+        modified_line = modify_line(line)
+        @parsed_stack << modified_line
+      end
+      @parsed_stack
+    end
+
+    def exclude_line?(line)
+      excluded = false
+      return false if @exclude.nil?
+      return false if @exclude.count.zero?
+
+      @exclude.each do |r|
+        excluded = true if line =~ r
+      end
+      excluded
+    end
+
+    def include_line?(line)
+      included = false
+      return false if @include.nil?
+      return false if @include.count.zero?
+
+      @include.each do |r|
+        included = true if line =~ r
+      end
+      included
+    end
+
+    def modify_line(line)
+      return line if @clean_keep.nil?
+      return line if @clean_keep.count.zero?
+
+      @clean_keep.each do |r|
+        m = line.match r
+        line = m[1] unless m.nil?
+      end
+
+      #method/line
+      # m = line.match(/^.*(\/\w+:\d+):.*/)
+      # line = m[1] unless m.nil?
+      # /api_vulnerabilities_filters_spec.rb:62:in
+
+
+      line
     end
   end
 end
--- spec/exstacktor_spec.rb
+++ spec/exstacktor_spec.rb
@@ -1,9 +1,56 @@
 RSpec.describe Exstacktor do
-  it "has a version number" do
+  it 'has a version number' do
     expect(Exstacktor::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  stacktrace1 = [
+    "/data/go-agent/pipelines/ec2_automation/lib/sendy_events_sender.rb:38:in `rescue in send_and_wait_for_event'",
+    "/data/agent/pipelines/ec2/lib/sendy_events_sender.rb:32:in `send_and_wait_for_event'",
+    "/data/agent/pipelines/ec2/regression_tests/new_overview/helpers/vulnerabilities_spec_helper.rb:71:in `send_vulnerabilities_event'",
+    "/data/agent/pipelines/ec2/spec/api/api_filters_spec.rb:62:in `block in send_vulnerabilities'",
+    "/data/agent/pipelines/ec2/spec/api/api_filters_spec.rb:60:in `each'",
+    "/data/agent/pipelines/ec2/spec/api/api_filters_spec.rb:60:in `send_vulnerabilities'",
+    "/data/agent/pipelines/ec2/spec/api/api_filters_spec.rb:143:in `block (3 levels) in \u003ctop (required)\u003e'",
+    "/data/tmp/ec2_automation/vendor/bundle/ruby/2.5.0/gems/rspec-core-3.8.2/lib/rspec/core/hook.rb:348:in `instance_exec'",
+    "/data/tmp/ec2_automation/vendor/bundle/ruby/2.5.0/gems/rspec-core-3.8.2/lib/rspec/core/hooks.rb:348:in `run'",
+    "/data/tmp/ec2_automation/vendor/bundle/ruby/2.5.0/gems/rspec-core-3.8.2/lib/rspec/core/hooks.rb:507:in `block in run_owned_hooks_for'",
+    "bin/rspec:29:in `load'"
+  ]
+
+  it 'removes lines that match an exclude' do
+    stack = Exstacktor::Stacktrace.new
+    stack.exclude = [/^.*rspec-core-.*/]
+    parsed = stack.parse stacktrace1
+    expect(parsed.count).to be 8
   end
+
+  it 'keeps exclude lines that match an include' do
+    stack = Exstacktor::Stacktrace.new
+    stack.exclude = [/^.*rspec-core-.*/]
+    stack.include = [/instance_exec/]
+    parsed = stack.parse stacktrace1
+    expect(parsed.count).to be 9
+  end
+
+  it "can set values through initializer" do
+    stack = Exstacktor::Stacktrace.new(exclude: [/^.*rspec-core-.*/], include: [/instance_exec/])
+    expect(stack.exclude).to eq [/^.*rspec-core-.*/]
+    expect(stack.include).to eq [/instance_exec/]
+    parsed = stack.parse stacktrace1
+    expect(parsed.count).to be 9
+  end
+
+  it 'can remove lines based on multiple exclusions' do
+    stack = Exstacktor::Stacktrace.new(exclude: [/^.*rspec-core-.*/, /sendy_events_sender/])
+    parsed = stack.parse stacktrace1
+    expect(parsed.count).to be 6
+  end
+
+  it 'can keep exclude lines based on multiple inclusions' do
+    stack = Exstacktor::Stacktrace.new(exclude: [/^.*rspec-core-.*/], include: [/instance_exec/, /hooks.rb:348/])
+    parsed = stack.parse stacktrace1
+    expect(parsed.count).to be 10
+  end
+
+
 end
